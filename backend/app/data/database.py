f1142a5a1b9bd18168d77c066902d5cdefd3c00f
Wesner Philogene <mrcodage@mrccommunity.com>
2025-12-16 20:28:34 -0500

    cleanup database.py

--- backend/app/data/database.py
+++ backend/app/data/database.py
@@ -4,14 +4,12 @@ from sqlalchemy.orm import sessionmaker
 import os
 from app.config.env import settings as sett
 
-# ========== URL DE LA BASE DE DONNEES ==========
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL", 
     sett.DATABASE_URL
 )
 
-# ========== CREER LE MOTEUR DE BASE DE DONNEES AVEC ENCODAGE UTF-8 ==========
 
 engine = create_engine(
     DATABASE_URL,
@@ -19,15 +17,9 @@ engine = create_engine(
     connect_args={"charset": "utf8mb4"}
 ) 
 
-# ========== CREER UNE SESSION ==========
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# ========== BASE POUR LES MODÈLES ==========
-
 Base = declarative_base()
-
-# ========== FONCTION POUR OBTENIR UNE SESSION DE BASE DE DONNEES ==========
 
 def get_db():
     db = SessionLocal()
