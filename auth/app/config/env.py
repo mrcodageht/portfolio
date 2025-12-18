8adc2f76144120da4bd4ae8f84ac395fbaaf74ba
Wesner Philogene <mrcodage@mrccommunity.com>
2025-12-17 23:20:12 -0500

    Modifications des fichiers env

--- auth/app/config/env.py
+++ auth/app/config/env.py
@@ -1,15 +1,14 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(extra="ignore", env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(extra="ignore", env_file=".env.auth", env_file_encoding="utf-8")
 
-    JWT_ALGORITHM: str = "HS256"
-    JWT_SECRET: str = "SECRET_TO_CHANGE"
+    JWT_ALGORITHM: str 
+    JWT_SECRET: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
-    AUTH_PROJECT_NAME: str = "Default Project Name"
-    AUTH_API_VERSION: str = "0.0.0"
-    EXPIRE: int = 30
+    AUTH_PROJECT_NAME: str 
+    AUTH_API_VERSION: str 
 
 settings = Settings()
 
--- backend/app/config/env.py
+++ backend/app/config/env.py
@@ -2,24 +2,25 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(extra="ignore", env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(extra="ignore", env_file=".env.backend", env_file_encoding="utf-8")
 
     DATABASE_URL: str
     SECRET_KEY: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     PROJECT_NAME: str
     API_VERSION: str
-    TOKEN_ENDPOINT: str = "http://auth-portfolio:8070/token"
-    TOKEN_VALIDATE_ENDPOINT: str = "http://auth-portfolio:8070/token/validate"
+
+    # container d'auth
+    TOKEN_ENDPOINT: str
+    TOKEN_VALIDATE_ENDPOINT: str 
 
 
     # admin
-    DEFAULT_ADMIN_LAST_NAME: str = "Default"
-    DEFAULT_ADMIN_FIRST_NAME: str = "Admin"
-    DEFAULT_ADMIN_EMAIL: str = "admin@example.com"
-    DEFAULT_ADMIN_PASSWORD: str = "Admin@12345"
+    DEFAULT_ADMIN_LAST_NAME: str
+    DEFAULT_ADMIN_FIRST_NAME: str
+    DEFAULT_ADMIN_EMAIL: str
+    DEFAULT_ADMIN_PASSWORD: str 
     
-    KEY_INIT: str = "494c157c-0880-461f-826a-3a867cfa128a"
+    KEY_INIT: str
 
 
 settings = Settings()
