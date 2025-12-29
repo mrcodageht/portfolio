196ae1220d37d9de2080f95deb84681453e60b59
Wesner Philogene <mrcodage@mrccommunity.com>
2025-12-29 11:51:20 -0500

    Gestion de l'expiration du token, resolution du crash du service d'auth lorsque la signature du token est expiree

--- auth/app/services_auth.py
+++ auth/app/services_auth.py
@@ -1,7 +1,7 @@
 import datetime
 
 from fastapi import HTTPException
-from jose import jwt
+from jose import ExpiredSignatureError, jwt
 from starlette import status
 from app.config.env import settings
 from app.token_schema import Token, TokenDecoded
@@ -17,10 +17,13 @@ def create_access_token(email: str, admin: bool = False) -> str:
     return jwt.encode(to_encode, settings.JWT_SECRET, algorithm=settings.JWT_ALGORITHM)
 
 def validate_token(token: str) -> TokenDecoded:
-    decode_result = jwt.decode(token, settings.JWT_SECRET, algorithms=[settings.JWT_ALGORITHM])
-    token_decoded = TokenDecoded(**decode_result)
+    try:
+        decode_result = jwt.decode(token, settings.JWT_SECRET, algorithms=[settings.JWT_ALGORITHM])
+        token_decoded = TokenDecoded(**decode_result)
 
-    if token_decoded.exp < datetime.datetime.now(datetime.timezone.utc):
+        if token_decoded.exp < datetime.datetime.now(datetime.timezone.utc):
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Expired token")
+
+        return token_decoded
+    except ExpiredSignatureError:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Expired token")
-
-    return token_decoded
